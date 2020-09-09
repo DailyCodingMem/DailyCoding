1d41f7144edffdb3a68ed7818844a1a90b65d0eb
장주환 <juhwan8956@naver.com>
2020-09-09 12:17:29 +0900

    0909

--- zjohn99/codeup/[문자열]1733_IOI.py
+++ zjohn99/codeup/[문자열]1733_IOI.py
@@ -6,3 +6,8 @@ sys.stdin = open("codeup/[문자열]1733_IOI.txt",'r')
 
 T = int(input())
 for _ in range(T):
+    word = input()
+    if word == 'IOI':
+        print("IOI is the International Olympiad in Informatics.")
+    else :
+        print("I don't care.")
