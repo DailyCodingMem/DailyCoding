9f7cd7e7ad63548f011d7ea6aec5be227da425b9
unknown <hh3990@naver.com>
2020-09-10 23:20:44 +0900

    0910

---
+++ JaehyukChoi923/codeup/빗금친사각형출력하기.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == 1 or i == n or j == 1 or j == n:
+            print('*', end='')
+        elif (i+j-1) % k == 0:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
