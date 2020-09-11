72b44d37348425ad317f149731f4fbedaa4d8a5d
장주환 <juhwan8956@naver.com>
2020-09-11 18:13:50 +0900

    codeup 200911

---
+++ zjohn99/codeup/[중첩반복문]1368_평행사변형출력2.py
@@ -0,0 +1,10 @@
+# codeup - 중첩반복문 - 평행사변형출력2 (2020-09-11)
+# https://codeup.kr/problem.php?id=1368
+
+import sys
+sys.stdin = open("codeup/[중첩반복문]1368_평행사변형출력2.txt",'r')
+
+T = int(input())
+for _ in range(T):
+    h, k, d = map(str, input().split())
+    
