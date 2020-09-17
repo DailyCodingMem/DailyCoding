103f5301bdc34eece6cc87cecdb5aaee02068c3b
장주환 <juhwan8956@naver.com>
2020-09-17 20:43:48 +0900

    0917

--- zjohn99/codeup/[BFS_DFS]3212_위상정렬.py
+++ zjohn99/codeup/[BFS_DFS]3212_위상정렬.py
@@ -1,9 +1,32 @@
 # codeup - BFS/DFS - 위상정렬 (2020-09-17)
 # https://codeup.kr/problem.php?id=3212
+# LG cns 문제랑 비슷
 
 import sys
 sys.stdin = open("codeup/[BFS_DFS]3212_위상정렬.txt",'r')
 
+def solution():
+    st = [1]
+    visited[1] = True
+
+    while(st) :
+
+        pass
+
+
+    return 0
+
+
 T = int(input())
 for _ in range(T):
-    pass
+    v, n = map(int, input().split())
+    graph = {}
+    visited = [False for _ in range(v+1)]
+    for i in range(n):
+        a, b = map(int, input().split())
+        if a not in graph :
+            graph[a] = [b]
+        else :
+            graph[a] += [b]
+
+    solution()
