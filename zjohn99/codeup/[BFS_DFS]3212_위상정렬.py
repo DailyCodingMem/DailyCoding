075dac14f65c9a01e610ade9b1802f9f482a6c2b
장주환 <juhwan8956@naver.com>
2020-09-17 20:30:50 +0900

    0917

---
+++ zjohn99/codeup/[BFS_DFS]3212_위상정렬.py
@@ -0,0 +1,9 @@
+# codeup - BFS/DFS - 위상정렬 (2020-09-17)
+# https://codeup.kr/problem.php?id=3212
+
+import sys
+sys.stdin = open("codeup/[BFS_DFS]3212_위상정렬.txt",'r')
+
+T = int(input())
+for _ in range(T):
+    pass
