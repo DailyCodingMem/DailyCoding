37fd8bc314713a83d357aa6cadb3fdf8501b309d
장주환 <juhwan8956@naver.com>
2020-09-21 23:32:05 +0900

    codeup

---
+++ zjohn99/codeup/[BFS_DFS]4023_오목.py
@@ -0,0 +1,25 @@
+# codeup - BFS/DFS - 오목 (2020-09-21)
+# https://codeup.kr/problem.php?id=4023
+
+
+import sys
+sys.stdin = open("codeup/[BFS_DFS]4023_오목.txt",'r')
+
+def solution():
+    st = [1]
+    visited[1] = True
+
+    while(st) :
+
+        pass
+
+
+    return 0
+
+
+T = int(input())
+for _ in range(T):
+
+
+
+    solution()
