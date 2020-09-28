589e51accc563a2aa68a467f5bebe7df20eb8e44
장주환 <juhwan8956@naver.com>
2020-09-28 22:44:54 +0900

    bfs_dfs graph

---
+++ zjohn99/codeup/[BFS_DFS]4503_바이러스.py
@@ -0,0 +1,48 @@
+# codeup - BFS/DFS - 바이러스 (2020-09-28)
+# https://codeup.kr/problem.php?id=4503
+
+import sys
+sys.stdin = open("codeup/[BFS_DFS]4503_바이러스.txt",'r')
+
+def solution():
+    st = [1]
+    cnt = 0
+    visited[1] = True
+    while(st) :
+        warm = st.pop(0)
+
+        if warm in gp :
+            tmp = gp[warm]
+            for i in tmp :
+                if visited[i] == False :
+                    st.append(i)
+                    visited[i] = True
+                    cnt += 1
+
+    return cnt
+
+
+        
+
+
+
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    s = int(input())
+    visited = [False for _ in range(n+1)]
+    gp = {}
+
+    for _ in range(s) :
+        a, b = map(int, input().split())
+        if a in gp :
+            gp[a] += [b]
+        else :
+            gp[a] = [b]
+
+        if b in gp :
+            gp[b] += [a]
+        else :
+            gp[b] = [a]
+    print(solution())
---
+++ zjohn99/codeup/[이분탐색]2633_LowerBound.py
@@ -0,0 +1,17 @@
+# codeup - 이분탐색 - LowerBound (2020-09-28)
+# https://codeup.kr/problem.php?id=2633
+
+import sys
+sys.stdin = open("codeup/[이분탐색]2633_LowerBound.txt",'r')
+
+
+def solution():
+   return 0
+
+    
+
+
+T = int(input())
+for _ in range(T):
+    s = input()
+    print(solution())
