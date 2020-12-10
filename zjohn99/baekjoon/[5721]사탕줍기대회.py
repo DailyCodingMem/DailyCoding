33e16d61e11a0e407bf0f23855ba6d63c1b4155f
장주환 <juhwan8956@naver.com>
2020-12-10 16:21:30 +0900

    그래프 기초

--- zjohn99/baekjoon/[5721]사탕줍기대회.py
+++ zjohn99/baekjoon/[5721]사탕줍기대회.py
@@ -6,8 +6,9 @@ sys.stdin = open("baekjoon/[5721]사탕줍기대회.txt",'r')
 
 T = int(input())
 for t in range(1, T+1):
-    M, N = map(int, input().split())
-    mat = []
-    if (M, N) != (0, 0) :
+    while(True) :    
+        M, N = map(int, input().split())
+        if (M, N) == (0,0) :
+            break
         mat = [list(map(int, input().split())) for _ in range(M)]
         
