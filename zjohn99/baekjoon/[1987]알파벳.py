2dd081908b121210ec36312418f898968df03306
장주환 <juhwan8956@naver.com>
2020-12-17 15:48:14 +0900

    재귀

--- zjohn99/baekjoon/[1987]알파벳.py
+++ zjohn99/baekjoon/[1987]알파벳.py
@@ -5,44 +5,30 @@ from collections import deque
 import sys
 sys.stdin = open("baekjoon/[1987]알파벳.txt",'r')
 
-def solution(y, x) :
-    dy = [-1, 0, 1, 0]
-    dx = [0, 1, 0, -1]
+def solution(y, x, cnt) :
+    global ans
 
-    st = deque([(y,x,1)])
-    visited = [[[] for _ in range(C)] for _ in range(R)]
-    visited[y][x] += mat[y][x]
-    # words = deque([mat[y][x]])
-    max_word = 0
-    while(st) :
-        y, x, cnt = st.popleft()
-        if max_word < cnt :
-            max_word = cnt
+    ans = max(ans, cnt)
 
-        # if len(visited[y][x]) < cnt :
-            
-        for i in range(4) :
-            ny = y + dy[i]
-            nx = x + dx[i]
-
-            if 0 <= ny < R and 0 <= nx < C :
-                if mat[ny][nx] not in visited[y][x] and len(visited[ny][nx]) < len(visited[y][x])+1:
-                    st.append((ny,nx,cnt+1))
-                    visited[ny][nx] += mat[ny][nx]
-                    visited[ny][nx] += visited[y][x]
-        #           
-        # else :
-        #     st.pop()
-        #     words.pop()
-    
-    return max_word
-
-
+    for i in range(4) :
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < R and 0 <= nx < C and not alpha[ord(mat[ny][nx]) - 65] :
+            alpha[ord(mat[ny][nx]) - 65] = 1
+            solution(ny, nx, cnt + 1)
+            alpha[ord(mat[ny][nx]) - 65] = 0
 
 
 T = int(input())
 for t in range(1, T+1):
     R, C = map(int, input().split())
     mat = [list(map(str,input())) for _ in range(R)]
-    
-    print(solution(0,0))
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
+    alpha = [0] * 26
+    alpha[ord(mat[0][0]) - 65] = 1
+    ans = 1
+
+    solution(0,0,1)
+
+    print(ans)
