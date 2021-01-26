d8385d0274246ba8104bcf536adcff9d05710b2a
장주환 <juhwan8956@naver.com>
2021-01-27 00:08:39 +0900

    0127

--- zjohn99/baekjoon/[2075]N번째큰수.py
+++ zjohn99/baekjoon/[2075]N번째큰수.py
@@ -8,7 +8,21 @@ sys.stdin = open("baekjoon/[2075]N번째큰수.txt",'r')
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
+    mat = []
+    for _ in range(N) :
+        mat += list(map(int, input().split()))
+    heapify(mat)
+    while(len(mat) > N): 
+        heappop(mat)
+
+    print(heappop(mat))
+   
+    # for i in range(len(mat)) :
+    #     mat[i] = -mat[i]
     
-    mat = list(input().split() for _ in range(N))
-    print(mat)
+    # heapify(mat)
+    # for i in range(N) :
+    #     tmp = heappop(mat)
+    #     if i == N-1 :
+    #         print(-tmp)
     
