a9965a8f05ef8d16eb88ee2fcacd1f36473dbcb2
장주환 <juhwan8956@naver.com>
2021-02-24 21:13:25 +0900

    0224

---
+++ zjohn99/baekjoon/[12100]2048.py
@@ -0,0 +1,14 @@
+# baekjoon - [12100]2048 (2021-02-24)
+# https://www.acmicpc.net/problem/12100
+
+from sys import stdin
+stdin = open("baekjoon/[12100]2048.txt",'r')
+
+T = int(stdin.readline())
+for t in range(1, T+1) :
+
+# from sys import stdin
+# n = int(stdin.readline()) # 이게 더 빠르다.
+
+
+# T = 
--- zjohn99/baekjoon/입출력속도향상.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.stdin = open("baekjoon/리스트회전.txt",'r')
-
-    
-T = int(input())
-for t in range(1, T+1):
