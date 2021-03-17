e20dcf13fc52cfcb84a17a544bb9b376c9ca703f
hongporo <sjsjsj717@naver.com>
2021-03-18 00:23:22 +0900

    programmers lv2 1 problem solve

--- jong0717/programmers/lv2/lv2-35_수식최대화.py
+++ jong0717/programmers/lv2/lv2-35_수식최대화.py
@@ -1,19 +1,61 @@
 from itertools import permutations as pmt
+def calculation(left,right,mathex):
+    if mathex == '+':
+        result = int(left) + int(right)
+    elif mathex == '-':
+        result = int(left) - int(right)
+    elif mathex == '*':
+        result = int(left) * int(right)
+    return result
+
 def solution(expression):
     cals = ["+","-","*"]
-    perms = pmt(cals,3)
-    stack = list(expression)
-
+    perms = list(pmt(cals,3))
+    mathBasket = []
+    exBasket = []
+    myDigit = ''
     answer = 0
+    for i in range(len(expression)):
+        if i == len(expression) - 1:
+            myDigit += expression[i]
+            myDigit = int(myDigit)
+            mathBasket.append(myDigit)
+        elif expression[i].isdigit():
+            myDigit += expression[i]
+        else:
+            myDigit = int(myDigit)
+            mathBasket.append(myDigit)
+            myDigit = ''
+            exBasket.append(expression[i])
+    for i in range(6):
+        cal = perms[i]
+        myExpress = exBasket[:]
+        myMath = mathBasket[:]
+        for j in range(3):
+            mathEx = cal[j]
+            while myExpress and mathEx in myExpress:
+                idx = myExpress.index(mathEx)
+                A = myMath.pop(idx)
+                B = myMath.pop(idx)
+                C = myExpress.pop(idx)
+                result = calculation(A,B,C)
+                myMath.insert(idx, result)
+        if answer < abs(myMath[0]):
+            answer = abs(myMath[0])
     return answer
-expression = "100-200*300-500+20"
-cals = ["+","-","*"]
-# stack = list(expression.split())
-perms = list(pmt(cals,3))
-for perm in perms:
-    print(perm[2])
-    print(expression.split('perm[2]'))
-    
-# print()
-print(perms)
-print(eval(expression))
+
+
+# 다른사람 풀이..
+def solution2(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            print(temp)
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
