6f169812da22b80880df4a5c31f0d9963ec2e8fc
hong <sjsjsj717@naver.com>
2021-04-27 17:15:21 +0900

    주석 코멘트 추가

--- jong0717/programmers/lv1/lv1-46_로또의최고순위와최저순위.py
+++ jong0717/programmers/lv1/lv1-46_로또의최고순위와최저순위.py
@@ -8,6 +8,7 @@ def Winning(n):
     return rank
 def solution(lottos, win_nums):
     # 일치하는 최대,최소 갯수를 저장할 리스트
+    # 애초에 최대에 해당하는 값을 count함수로 0의 갯수를 저장해두면 elif절은 필요없음.
     result = [0,0]
     for lotto in lottos:
         # 일치하는 숫자 갯수를 최대,최소에 우선 저장
