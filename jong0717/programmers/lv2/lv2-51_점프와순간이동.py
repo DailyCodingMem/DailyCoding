1953fb88d7797b0bb84ebd44de2d4764122cd26d
hong <sjsjsj717@naver.com>
2021-05-01 17:25:49 +0900

    programmers lv2 1 problem update

--- jong0717/programmers/lv2/lv2-51_점프와순간이동.py
+++ jong0717/programmers/lv2/lv2-51_점프와순간이동.py
@@ -1,9 +1,10 @@
 def solution(n):
     ans = 0
-    
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    while n > 0:
+        q, r = divmod(n, 2)
+        n = q
+        if r != 0 :
+            ans += 1
     return ans
 
 
